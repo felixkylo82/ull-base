6cc81f3e62a7bd78caef6f763be0a3d2519414c3
Felix Kui-Yip LO <felixkylo82@gmail.com>
2015-01-07 13:55:25 +0800

    Update queue.h

--- utils/src/queue.h
+++ utils/src/queue.h
@@ -8,8 +8,31 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
 
+template<typename Item>
+class QueueNode {
+private:
+    Item* item;
+    const QueueNode* next;
+    
+public:
+    QueueNode(Item* item, const QueueNode* next);
+    virtual ~QueueNode();
+};
 
+template<typename Item>
+class Queue {
+private:
+	const QueueNode<Item>* head;
+	const QueueNode<Item>* tail;
+	
+public:
+    Queue();
+    virtual ~Queue();
+    
+    void push(Item* item);
+    Item* pop();
+};
 
-
+#include "queue.cpp"
 
 #endif /* QUEUE_H_ */
